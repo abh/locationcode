ba0a72715024fdcd916549ac48dc2b85e3c12043
Ask Bjørn Hansen <ask@develooper.com>
2022-05-01 12:20:23 -0700

    Ensure non-US countries with less airport codes get sufficient choices

--- airports.go
+++ airports.go
@@ -24,6 +24,10 @@ type Airport struct {
 	data     *alphafoxtrot.Airport
 }
 
+type Finder struct {
+	f *alphafoxtrot.AirportFinder
+}
+
 func main() {
 
 	var dataDir = flag.String("data-dir", "./data", "Data cache directory")
@@ -32,7 +36,9 @@ func main() {
 
 	validateData(*dataDir)
 
-	finder := alphafoxtrot.NewAirportFinder()
+	finder := &Finder{
+		f: alphafoxtrot.NewAirportFinder(),
+	}
 
 	// LoadOptions come with preset filepaths
 	options := alphafoxtrot.PresetLoadOptions(*dataDir)
@@ -41,8 +47,45 @@ func main() {
 	filter := alphafoxtrot.AirportTypeRunways
 
 	// Load the data into memory
-	if err := finder.Load(options, filter); len(err) > 0 {
+	if err := finder.f.Load(options, filter); len(err) > 0 {
 		log.Println("errors:", err)
+	}
+
+	if args := flag.Args(); len(args) > 0 {
+		if len(args) != 4 {
+			fmt.Printf("[cc] [lat] [lng] [radius]\n")
+			os.Exit(2)
+		}
+		cc := strings.ToUpper(args[0])
+
+		latitude, err := strconv.ParseFloat(args[1], 64)
+		if err != nil {
+			fmt.Printf("could not parse %s as latitude: %s", args[1], err)
+			os.Exit(2)
+		}
+
+		longitude, err := strconv.ParseFloat(args[2], 64)
+		if err != nil {
+			fmt.Printf("could not parse %s as longitude: %s", args[2], err)
+			os.Exit(2)
+		}
+
+		radiusKM, err := strconv.ParseFloat(args[3], 64)
+		if err != nil {
+			fmt.Printf("could not parse %s as radius: %s", args[3], err)
+			os.Exit(2)
+		}
+
+		airports, err := finder.GetAirports(cc, radiusKM, latitude, longitude)
+		if err != nil {
+			fmt.Printf("GetAirports error: %s\n", err)
+		}
+
+		for _, a := range airports {
+			fmt.Printf("%s\t%s (%0f)\n", a.Code, a.Name, a.Distance)
+		}
+
+		os.Exit(0)
 	}
 
 	e := echo.New()
@@ -63,13 +106,15 @@ func main() {
 
 		radiusString := c.QueryParam("radius")
 
-		var radiusKM int
+		var radiusKM float64
 		if len(radiusString) > 0 {
-			radiusKM, _ = strconv.Atoi(radiusString)
+			radiusKM, _ = strconv.ParseFloat(radiusString, 64)
 		}
 
-		if radiusKM < 10 {
-			radiusKM = 100
+		if radiusKM < 150 {
+			radiusKM = 150
+		} else {
+			radiusKM = radiusKM * 1.5
 		}
 
 		latitude, longitude, err := getLatLng(c)
@@ -77,62 +122,80 @@ func main() {
 			return c.String(http.StatusBadRequest, fmt.Sprintf("invalid lat or lng: %s", err))
 		}
 
-		ipLocation := s2.LatLngFromDegrees(latitude, longitude)
-
-		maxResults := 100
-		radiusInMeters := float64(radiusKM) * 1000
-		airportsRaw := finder.FindNearestAirportsByCountry(countryISOCode, latitude, longitude, radiusInMeters, maxResults, filter)
-
-		airports := []*alphafoxtrot.Airport{}
-		for _, ap := range airportsRaw {
-			if len(ap.IATACode) == 0 {
-				continue
-			}
-			airports = append(airports, ap)
+		airports, err := finder.GetAirports(countryISOCode, radiusKM, latitude, longitude)
+		if err != nil {
+			return err
 		}
 
-		llCache := map[int]s2.LatLng{}
-		for i, ap := range airports {
-			ll := s2.LatLngFromDegrees(ap.LatitudeDeg, ap.LongitudeDeg)
-			llCache[i] = ll
-		}
-
-		r := []*Airport{}
-
-		for i, airport := range airports {
-			// fmt.Printf("%d %s: %+v\n", i, airport.Name, airport)
-
-			code := strings.ToLower(airport.Country.ISOCode + airport.IATACode)
-
-			distance := float64(ipLocation.Distance(llCache[i])) * 6371.01
-
-			a := &Airport{
-				Name:     airport.Name,
-				Code:     code,
-				Distance: distance,
-				data:     airport,
-			}
-			r = append(r, a)
-		}
-
-		sort.Slice(r, func(i, j int) bool {
-			if r[i].data.Type == r[j].data.Type {
-				return r[i].Distance < r[j].Distance
-			}
-			return airports[i].Type < airports[j].Type
-		})
-
-		if len(r) > 10 {
-			r = r[0:10]
-		}
-
-		fmt.Printf("got %d airports, filtered to %d, returning %d\n", len(airportsRaw), len(airports), len(r))
-
-		return c.JSON(http.StatusOK, r)
+		return c.JSON(http.StatusOK, airports)
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
 
+}
+
+func (f *Finder) GetAirports(cc string, radiusKM, latitude, longitude float64) ([]*Airport, error) {
+
+	log.Printf("GetAirports(%s %.2f %.4f %.4f)", cc, radiusKM, latitude, longitude)
+
+	ipLocation := s2.LatLngFromDegrees(latitude, longitude)
+
+	maxResults := 500 // some countries have a lot of airports without local codes
+	radiusInMeters := radiusKM * 1000
+	airportsRaw := f.f.FindNearestAirportsByCountry(
+		cc,
+		latitude, longitude,
+		radiusInMeters,
+		maxResults,
+		alphafoxtrot.AirportTypeAll, // filtered at load time
+	)
+
+	airports := []*alphafoxtrot.Airport{}
+	for _, ap := range airportsRaw {
+		if len(ap.IATACode) == 0 {
+			continue
+		}
+		airports = append(airports, ap)
+	}
+
+	llCache := map[int]s2.LatLng{}
+	for i, ap := range airports {
+		ll := s2.LatLngFromDegrees(ap.LatitudeDeg, ap.LongitudeDeg)
+		llCache[i] = ll
+	}
+
+	r := []*Airport{}
+
+	for i, airport := range airports {
+		fmt.Printf("%d %s: %+v\n", i, airport.Name, airport)
+
+		code := strings.ToLower(airport.Country.ISOCode + airport.IATACode)
+
+		distance := float64(ipLocation.Distance(llCache[i])) * 6371.01
+
+		a := &Airport{
+			Name:     airport.Name,
+			Code:     code,
+			Distance: distance,
+			data:     airport,
+		}
+		r = append(r, a)
+	}
+
+	sort.Slice(r, func(i, j int) bool {
+		if r[i].data.Type == r[j].data.Type {
+			return r[i].Distance < r[j].Distance
+		}
+		return airports[i].Type < airports[j].Type
+	})
+
+	if len(r) > 15 {
+		r = r[0:15]
+	}
+
+	fmt.Printf("got %d airports, filtered to %d, returning %d\n", len(airportsRaw), len(airports), len(r))
+
+	return r, nil
 }
 
 func validateData(dataDir string) {
--- go.mod
+++ go.mod
@@ -16,9 +16,9 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
-	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
-	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
+	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
 )
